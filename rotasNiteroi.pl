5f476b7e1a0469480338d28901875dcbd9caad73
Matheus <90159525+oftheus@users.noreply.github.com>
2025-01-04 13:53:28 -0300

    Criação dos predicados de conexão entre cruzamentos com ruas

---
+++ rotasNiteroi.pl
@@ -0,0 +1,294 @@
+conexao(C1, C2, "Av. Jorn. Alberto Franciso Torres", 150).
+conexao(C2, C1, "Av. Jorn. Alberto Franciso Torres", 150).
+
+conexao(C2, C3, "Av. Jorn. Alberto Franciso Torres", 120).
+conexao(C3, C2, "Av. Jorn. Alberto Franciso Torres", 120).
+
+conexao(C3, C4, "Av. Jorn. Alberto Franciso Torres", 150).
+conexao(C4, C3, "Av. Jorn. Alberto Franciso Torres", 150).
+
+conexao(C4, C5, "Av. Jorn. Alberto Franciso Torres", 150).
+conexao(C5, C4, "Av. Jorn. Alberto Franciso Torres", 150).
+
+conexao(C5, C6, "Av. Jorn. Alberto Franciso Torres", 150).
+conexao(C6, C5, "Av. Jorn. Alberto Franciso Torres", 150).
+
+conexao(C6, C7, "Av. Jorn. Alberto Franciso Torres", 120).
+conexao(C7, C6, "Av. Jorn. Alberto Franciso Torres", 120).
+
+conexao(C7, C8, "Av. Jorn. Alberto Franciso Torres", 130).
+conexao(C8, C7, "Av. Jorn. Alberto Franciso Torres", 130).
+
+conexao(C8, C9, "Av. Jorn. Alberto Franciso Torres", 120).
+conexao(C9, C8, "Av. Jorn. Alberto Franciso Torres", 120).
+
+conexao(C9, C10, "Av. Jorn. Alberto Franciso Torres", 140).
+conexao(C10, C9, "Av. Jorn. Alberto Franciso Torres", 140).
+
+conexao(C10, C11, "Av. Jorn. Alberto Franciso Torres", 140).
+conexao(C11, C10, "Av. Jorn. Alberto Franciso Torres", 140).
+
+
+
+conexao(C13, C1, "R. Miguel de Frias", 200).
+
+conexao(C14, C13, "R. Miguel de Frias", 150).
+
+conexao(C15, C14, "R. Miguel de Frias", 160).
+
+conexao(C16, C15, "R. Miguel de Frias", 150).
+
+conexao(C17, C16, "R. Miguel de Frias", 130).
+
+
+
+conexao(C22, C23, "R. Castilho França", 120).
+
+
+
+conexao(C69, C17, "R. Dr. Paulo César", 180).
+
+conexao(C71, C69, "R. Dr. Paulo César", 150).
+
+conexao(C74, C71, "R. Dr. Paulo César", 180).
+
+
+
+conexao(C2, C18, "R. Álvares de Azevedo", 170).
+
+conexao(C18, C19, "R. Álvares de Azevedo", 150).
+
+conexao(C19, C20, "R. Álvares de Azevedo", 150).
+
+conexao(C20, C21, "R. Álvares de Azevedo", 130).
+
+conexao(C21, C24, "R. Álvares de Azevedo", 120).
+
+conexao(C24, C69, "R. Álvares de Azevedo", 120).
+
+
+
+conexao(C29, C3, "R. Gen. Pereira da Silva", 170).
+
+conexao(C28, C29, "R. Gen. Pereira da Silva", 140).
+
+conexao(C27, C28, "R. Gen. Pereira da Silva", 150).
+
+conexao(C26, C27, "R. Gen. Pereira da Silva", 140).
+
+conexao(C25, C26, "R. Gen. Pereira da Silva", 110).
+
+conexao(C70, C25, "R. Gen. Pereira da Silva", 110).
+
+conexao(C71, C70, "R. Gen. Pereira da Silva", 120).
+
+
+
+conexao(C4, C30, "R. Pres. Backer", 170).
+
+conexao(C30, C31, "R. Pres. Backer", 130).
+
+conexao(C31, C32, "R. Pres. Backer", 110).
+
+conexao(C32, C33, "R. Pres. Backer", 140).
+
+conexao(C33, C34, "R. Pres. Backer", 120).
+
+conexao(C34, C72, "R. Pres. Backer", 130).
+
+conexao(C72, C73, "R. Pres. Backer", 110).
+
+conexao(C73, C74, "R. Pres. Backer", 90).
+
+conexao(C74, C76, "R. Pres. Backer", 75).
+
+conexao(C76, C78, "R. Pres. Backer", 90).
+
+
+
+conexao(C5, C35, "R. Cap. Zeferino", 100).
+
+
+
+conexao(C5, C35, "R. Lopes Trovão", 170).
+
+conexao(C35, C36, "R. Lopes Trovão", 130).
+
+conexao(C36, C39, "R. Lopes Trovão", 110).
+
+conexao(C39, C40, "R. Lopes Trovão", 150).
+
+conexao(C40, C41, "R. Lopes Trovão", 130).
+
+conexao(C41, C83, "R. Lopes Trovão", 120).
+
+conexao(C83, C84, "R. Lopes Trovão", 100).
+
+conexao(C84, C75, "R. Lopes Trovão", 110).
+
+conexao(C75, C77, "R. Lopes Trovão", 90).
+
+conexao(C77, C79, "R. Lopes Trovão", 80).
+
+conexao(C79, C80, "R. Lopes Trovão", 90).
+
+
+
+conexao(C6, C42, "R. Otávio Carneiro", 170).
+
+conexao(C42, C43, "R. Otávio Carneiro", 140).
+
+conexao(C43, C44, "R. Otávio Carneiro", 120).
+
+
+
+conexao(C6, C42, "R. Otávio Carneiro", 170).
+
+conexao(C42, C43, "R. Otávio Carneiro", 140).
+
+conexao(C43, C44, "R. Otávio Carneiro", 120).
+
+
+
+conexao(C85, C81, "R. Dr. Herotides de Oliveira", 130).
+
+conexao(C86, C85, "R. Dr. Herotides de Oliveira", 110).
+
+conexao(C87, C86, "R. Dr. Herotides de Oliveira", 105).
+
+
+
+conexao(C88, C87, "Tv. Dr. César Couple", 120).
+
+
+
+conexao(C82, C90, "R. Prof. Miguel Couto", 130).
+
+conexao(C90, C91, "R. Prof. Miguel Couto", 100).
+
+conexao(C91, C92, "R. Prof. Miguel Couto", 110).
+
+conexao(C92, C93, "R. Prof. Miguel Couto", 110).
+
+conexao(C93, C89, "R. Prof. Miguel Couto", 100).
+
+
+
+conexao(C7, C45, "R. Belisário Augusto", 170).
+
+
+
+conexao(C50, C49, "R. Domingues de Sá", 100).
+
+conexao(C51, C50, "R. Domingues de Sá", 90).
+
+conexao(C94, C51, "R. Domingues de Sá", 110).
+
+conexao(C95, C94, "R. Domingues de Sá", 90).
+
+conexao(C96, C95, "R. Domingues de Sá", 100).
+
+conexao(C97, C96, "R. Domingues de Sá", 145).
+
+conexao(C98, C97, "R. Domingues de Sá", 95).
+
+
+
+conexao(C46, C8, "R. Osvaldo Cruz", 180).
+
+
+
+conexao(C52, C53, "R. Cinco de Julho", 90).
+
+conexao(C53, C54, "R. Cinco de Julho", 100).
+
+conexao(C54, C99, "R. Cinco de Julho", 120).
+
+conexao(C99, C100, "R. Cinco de Julho", 110).
+
+conexao(C100, C101, "R. Cinco de Julho", 120).
+
+conexao(C101, C102, "R. Cinco de Julho", 140).
+
+conexao(C102, C103, "R. Cinco de Julho", 100).
+
+
+
+conexao(C9, C47, "R. Mariz e Barros", 190).
+
+conexao(C47, C48, "R. Mariz e Barros", 300).
+
+conexao(C48, C61, "R. Mariz e Barros", 40).
+
+conexao(C61, C55, "R. Mariz e Barros", 30).
+
+conexao(C55, C56, "R. Mariz e Barros", 130).
+
+conexao(C56, C104, "R. Mariz e Barros", 120).
+
+conexao(C104, C105, "R. Mariz e Barros", 120).
+
+conexao(C105, C106, "R. Mariz e Barros", 110).
+
+conexao(C106, C107, "R. Mariz e Barros", 140).
+
+conexao(C107, C108, "R. Mariz e Barros", 100).
+
+
+
+conexao(C57, C10, "R. Comendador Queirós", 190).
+
+
+
+conexao(C57, C58, "Alameda Carolina", 280).
+
+
+
+conexao(C59, C58, "Av. Sete de Setembro", 40).
+
+conexao(C60, C59, "Av. Sete de Setembro", 30).
+
+conexao(C61, C60, "Av. Sete de Setembro", 120).
+
+conexao(C109, C61, "Av. Sete de Setembro", 110).
+
+conexao(C110, C109, "Av. Sete de Setembro", 100).
+
+conexao(C111, C110, "Av. Sete de Setembro", 90).
+
+conexao(C112, C111, "Av. Sete de Setembro", 165).
+
+conexao(C113, C112, "Av. Sete de Setembro", 110).
+
+
+
+conexao(C63, C64, "R. Dr. Carlos Halfeld", 110).
+
+conexao(C64, C65, "R. Dr. Carlos Halfeld", 110).
+
+
+
+conexao(C65, C114, "Rua Francisco Dutra", 110).
+
+conexao(C114, C115, "Rua Francisco Dutra", 160).
+
+
+
+conexao(C11, C62, "Av. Alm. Ary Parreiras", 250).
+conexao(C62, C11, "Av. Alm. Ary Parreiras", 250).
+
+conexao(C62, C66, "Av. Alm. Ary Parreiras", 270).
+conexao(C66, C62, "Av. Alm. Ary Parreiras", 270).
+
+conexao(C66, C67, "Av. Alm. Ary Parreiras", 100).
+conexao(C67, C66, "Av. Alm. Ary Parreiras", 100).
+
+conexao(C67, C68, "Av. Alm. Ary Parreiras", 110).
+
+conexao(C68, C116, "Av. Alm. Ary Parreiras", 130).
+
+conexao(C116, C117, "Av. Alm. Ary Parreiras", 170).
+
+conexao(C117, C118, "Av. Alm. Ary Parreiras", 190).
+
+conexao(C118, C119, "Av. Alm. Ary Parreiras", 120).
