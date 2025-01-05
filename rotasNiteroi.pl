373730ed122633fe7b32fd32f19207d1ffbb21e3
Matheus <90159525+oftheus@users.noreply.github.com>
2025-01-05 09:46:35 -0300

    Complementando os predicados de conexão entre cruzamentos com ruas

--- rotasNiteroi.pl
+++ rotasNiteroi.pl
@@ -106,7 +106,7 @@ conexao(C76, C78, "R. Pres. Backer", 90).
 
 
 
-conexao(C5, C35, "R. Cap. Zeferino", 100).
+conexao(C38, C37, "R. Cap. Zeferino", 100).
 
 
 
@@ -158,7 +158,7 @@ conexao(C87, C86, "R. Dr. Herotides de Oliveira", 105).
 
 
 
-conexao(C88, C87, "Tv. Dr. César Couple", 120).
+conexao(C87, C88, "Tv. Dr. César Couple", 120).
 
 
 
@@ -248,9 +248,9 @@ conexao(C59, C58, "Av. Sete de Setembro", 40).
 
 conexao(C60, C59, "Av. Sete de Setembro", 30).
 
-conexao(C61, C60, "Av. Sete de Setembro", 120).
+conexao(C161, C60, "Av. Sete de Setembro", 120).
 
-conexao(C109, C61, "Av. Sete de Setembro", 110).
+conexao(C109, C161, "Av. Sete de Setembro", 110).
 
 conexao(C110, C109, "Av. Sete de Setembro", 100).
 
@@ -292,3 +292,223 @@ conexao(C116, C117, "Av. Alm. Ary Parreiras", 170).
 conexao(C117, C118, "Av. Alm. Ary Parreiras", 190).
 
 conexao(C118, C119, "Av. Alm. Ary Parreiras", 120).
+
+
+
+conexao(C13, C18, "R. Ator Paulo Gustavo", 150).
+
+conexao(C18, C29, "R. Ator Paulo Gustavo", 120).
+
+conexao(C29, C30, "R. Ator Paulo Gustavo", 150).
+
+conexao(C30, C35, "R. Ator Paulo Gustavo", 150).
+
+conexao(C35, C42, "R. Ator Paulo Gustavo", 150).
+
+conexao(C42, C45, "R. Ator Paulo Gustavo", 120).
+
+conexao(C45, C46, "R. Ator Paulo Gustavo", 130).
+
+conexao(C46, C47, "R. Ator Paulo Gustavo", 120).
+
+conexao(C47, C57, "R. Ator Paulo Gustavo", 140).
+
+conexao(C62, C57, "R. Ator Paulo Gustavo", 140).
+
+
+
+conexao(C19, C14, "Rua Dr. Tavares de Macedo", 150).
+
+conexao(C28, C19, "Rua Dr. Tavares de Macedo", 120).
+
+conexao(C31, C28, "Rua Dr. Tavares de Macedo", 150).
+
+conexao(C37, C31, "Rua Dr. Tavares de Macedo", 80).
+
+conexao(C36, C37, "Rua Dr. Tavares de Macedo", 70).
+
+conexao(C43, C36, "Rua Dr. Tavares de Macedo", 150).
+
+
+
+conexao(C15, C20, "R. Gavião Peixoto", 160).
+
+conexao(C20, C27, "R. Gavião Peixoto", 130).
+
+conexao(C27, C32, "R. Gavião Peixoto", 150).
+
+conexao(C32, C38, "R. Gavião Peixoto", 70).
+
+conexao(C38, C39, "R. Gavião Peixoto", 80).
+
+conexao(C39, C44, "R. Gavião Peixoto", 160).
+
+conexao(C44, C49, "R. Gavião Peixoto", 130).
+
+conexao(C49, C52, "R. Gavião Peixoto", 120).
+
+conexao(C52, C48, "R. Gavião Peixoto", 130).
+
+conexao(C48, C58, "R. Gavião Peixoto", 140).
+
+conexao(C58, C63, "R. Gavião Peixoto", 110).
+
+conexao(C63, C66, "R. Gavião Peixoto", 100).
+
+
+
+conexao(C16, C23, "R. Mem de Sá", 70).
+
+conexao(C23, C21, "R. Mem de Sá", 80).
+
+conexao(C21, C26, "R. Mem de Sá", 120).
+
+conexao(C26, C33, "R. Mem de Sá", 140).
+
+conexao(C33, C40, "R. Mem de Sá", 150).
+
+
+
+conexao(C61, C59, "R. Dr. Miguel Viêira Ferreira", 130).
+
+
+
+conexao(C50, C53, "Rua Lemos Cunha", 120).
+
+conexao(C53, C55, "Rua Lemos Cunha", 130).
+
+conexao(C55, C60, "Rua Lemos Cunha", 120).
+
+conexao(C60, C64, "Rua Lemos Cunha", 100).
+
+conexao(C64, C67, "Rua Lemos Cunha", 100).
+
+
+
+conexao(C22, C17, "Av. Roberto Silveira", 80).
+
+conexao(C24, C22, "Av. Roberto Silveira", 70).
+
+conexao(C25, C24, "Av. Roberto Silveira", 110).
+
+conexao(C34, C25, "Av. Roberto Silveira", 150).
+
+conexao(C41, C34, "Av. Roberto Silveira", 130).
+
+conexao(C81, C41, "Av. Roberto Silveira", 80).
+
+conexao(C82, C81, "Av. Roberto Silveira", 90).
+
+conexao(C51, C82, "Av. Roberto Silveira", 90).
+
+conexao(C54, C51, "Av. Roberto Silveira", 110).
+
+conexao(C56, C54, "Av. Roberto Silveira", 120).
+
+conexao(C161, C56, "Av. Roberto Silveira", 130).
+
+conexao(C65, C161, "Av. Roberto Silveira", 90).
+
+conexao(C68, C65, "Av. Roberto Silveira", 110).
+
+
+
+conexao(C69, C70, "R. Mario Alves", 100).
+
+conexao(C70, C72, "R. Mario Alves", 110).
+
+
+
+conexao(C83, C85, "R. Min. Otávio Kelly", 90).
+
+conexao(C85, C90, "R. Min. Otávio Kelly", 110).
+
+conexao(C90, C94, "R. Min. Otávio Kelly", 100).
+
+conexao(C94, C99, "R. Min. Otávio Kelly", 135).
+
+conexao(C99, C104, "R. Min. Otávio Kelly", 130).
+
+conexao(C104, C109, "R. Min. Otávio Kelly", 140).
+
+conexao(C109, C114, "R. Min. Otávio Kelly", 110).
+
+conexao(C114, C116, "R. Min. Otávio Kelly", 120).
+
+
+
+conexao(C73, C71, "R. Santos Dumont", 130).
+
+
+
+conexao(C84, C86, "R. Nóbrega", 105).
+
+conexao(C86, C91, "R. Nóbrega", 100).
+
+conexao(C91, C95, "R. Nóbrega", 100).
+
+conexao(C95, C100, "R. Nóbrega", 120).
+
+conexao(C100, C105, "R. Nóbrega", 110).
+
+conexao(C105, C110, "R. Nóbrega", 110).
+
+
+
+conexao(C75, C74, "R. Pres. João Pessoa", 120).
+
+conexao(C87, C75, "R. Pres. João Pessoa", 90).
+
+conexao(C92, C87, "R. Pres. João Pessoa", 100).
+
+conexao(C96, C92, "R. Pres. João Pessoa", 105).
+
+conexao(C101, C96, "R. Pres. João Pessoa", 130).
+
+conexao(C106, C101, "R. Pres. João Pessoa", 140).
+
+conexao(C111, C106, "R. Pres. João Pessoa", 120).
+
+conexao(C115, C111, "R. Pres. João Pessoa", 90).
+
+conexao(C117, C115, "R. Pres. João Pessoa", 100).
+
+
+
+conexao(C77, C76, "R. Ten. Mesquita", 110).
+
+
+
+conexao(C78, C79, "R. Geraldo Martins", 110).
+
+conexao(C79, C88, "R. Geraldo Martins", 80).
+
+conexao(C88, C93, "R. Geraldo Martins", 70).
+
+conexao(C93, C97, "R. Geraldo Martins", 90).
+
+conexao(C97, C102, "R. Geraldo Martins", 115).
+
+conexao(C102, C107, "R. Geraldo Martins", 120).
+
+conexao(C107, C112, "R. Geraldo Martins", 120).
+
+
+
+conexao(C112, C118, "R. Dom Bosco", 145).
+
+
+
+conexao(C119, C113, "R. Santa Rosa", 140).
+
+conexao(C113, C108, "R. Santa Rosa", 110).
+
+conexao(C108, C103, "R. Santa Rosa", 90).
+
+conexao(C103, C98, "R. Santa Rosa", 110).
+
+conexao(C98, C89, "R. Santa Rosa", 80).
+
+conexao(C89, C80, "R. Santa Rosa", 130).
+
+conexao(C80, C74, "R. Santa Rosa", 210).
